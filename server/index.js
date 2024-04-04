8c31d17f395f5c865060bd4f56448060c303fc39
jo-oo <johannakoreo@gmail.com>
2024-04-04 19:08:51 +0200

    link index.html to index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import { createServer } from 'node:http';
+//for index.html
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 
+//setting up a server using Express
 const app = express();
 const server = createServer(app);
 
+//index.html connection
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+//running request to server
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
