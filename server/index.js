7d58a0d20a74a4a3b217399ded15dc02de4efa75
jo-oo <johannakoreo@gmail.com>
2024-04-04 19:33:28 +0200

    setting up socket.io and listen for incoming events

--- server/index.js
+++ server/index.js
@@ -3,10 +3,14 @@ import { createServer } from 'node:http';
 //for index.html
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
+//socket.io
+import { Server } from 'socket.io';
 
 //setting up a server using Express
 const app = express();
 const server = createServer(app);
+//setting upp socket.io with the server
+const io = new Server(server);
 
 //index.html connection
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -15,11 +19,12 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
-//running request to server
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+//listening for incoming events using socket.io
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
+//server request running on port 3000
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
